293c8374139b8822c771dcb685ed1b6a9c7b3d66
khanhluu <khanhluu@beeketing.net>
2023-06-20 00:26:57 +0700

    Update https

--- api/src/app.js
+++ api/src/app.js
@@ -1,4 +1,7 @@
 import express from 'express';
+import https from 'https';
+import http from 'http';
+import * as fs from 'fs';
 import { port } from './config/index.js';
 import loader from './loaders/index.js';
 
@@ -7,12 +10,18 @@ const app = express();
 loader(app);
 
 // eslint-disable-next-line consistent-return
-app.listen(port, (err) => {
-  if (err) {
-    console.log(err);
-    return process.exit(1);
-  }
-  console.log(`Server is running on ${port}`);
-});
+// app.listen(port, (err) => {
+//   if (err) {
+//     console.log(err);
+//     return process.exit(1);
+//   }
+//   console.log(`Server is running on ${port}`);
+// });
+
+http.createServer({}, app).listen(port);
+https.createServer({
+  key: fs.readFileSync('key.pem'),
+  cert: fs.readFileSync('cert.pem'),
+}, app).listen(443);
 
 export default app;
