a6e5eb9836027836d5fb6fb9a1ad9afdf99c0ebc
khanhluu <khanhluu@beeketing.net>
2023-06-21 18:16:44 +0700

    Update cart

--- api/src/controllers/shopee/cartRoute.js
+++ api/src/controllers/shopee/cartRoute.js
@@ -174,15 +174,15 @@ export async function deleteCartItems(req, res) {
     return res.status(400).json(errorHelper('00080', req, 'Invalid request'));
   }
 
-  if (!req.isOwner) {
-    const cartItem = await CartItem.findOneAndDelete({
-      _id: id,
-      authKey: req.authKey,
-    });
-    if (!cartItem) {
-      return res.status(400).json(errorHelper('00080', req, 'Invalid request'));
-    }
-  }
+  // if (!req.isOwner) {
+  //   const cartItem = await CartItem.findOneAndDelete({
+  //     _id: id,
+  //     authKey: req.authKey,
+  //   });
+  //   if (!cartItem) {
+  //     return res.status(400).json(errorHelper('00080', req, 'Invalid request'));
+  //   }
+  // }
 
   await Cart.findOneAndUpdate({
     _id: id,
